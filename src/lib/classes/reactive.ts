557421e6d46a768b2fb704d138d2efc33586520b
Gustav Eikaas <gustav.eikaas@gmail.com>
2022-12-03 19:23:25 +0100

    testing new token

--- src/lib/classes/reactive.ts
+++ src/lib/classes/reactive.ts
@@ -33,6 +33,7 @@ export class Reactive<TValue> implements ReactiveTS<TValue> {
 			return;
 		}
 		this.value = value;
+		
 		this.#onChangeCallbacks.forEach(({ callback }) => callback(value, oldValue));
 	};
 
